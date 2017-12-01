b0ad433d6a9c9911b6e04675733c5f40d3c14c3a
guominglong <guominglong@51talk.com>
2017-12-01 11:08:34 +0800

    添加H5互动教材设计器项目

---
+++ chachaH5Design/chachaH5Design/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  chachaH5Design
+//
+//  Created by guominglong on 2017/12/1.
+//  Copyright © 2017年 SSdk. All rights reserved.
+//
+
+import Cocoa
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ chachaH5Design/chachaH5Design/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  chachaH5Design
+//
+//  Created by guominglong on 2017/12/1.
+//  Copyright © 2017年 SSdk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
