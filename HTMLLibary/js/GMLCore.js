a4fc1640e19f588cb791ca8c2057d7f10a13f3df
guominglong <guominglong@51talk.com>
2017-12-26 19:25:26 +0800

    完善系统类型判断逻辑

--- HTMLLibary/js/GMLCore.js
+++ HTMLLibary/js/GMLCore.js
@@ -53,6 +53,14 @@ class BaseScene extends BaseObject{
         this._defaultOverDisItem = null;//鼠标移入时的响应可视化对象
     }
 
+    width(){
+        return this._mainCanvas.width;
+    }
+
+    height(){
+        return this._mainCanvas.height;
+    }
+
     start(){
         //将画布添加至document
         document.body.appendChild(this._mainCanvas.canvas);
@@ -1353,7 +1361,7 @@ class GMLStaticTextField extends GMLDisplay{
             this._isTextChanged = false;
         }
         let tempYOffset = 0;
-        if(OSManager.OS == "mac")
+        if(OSManager.OS == "Mac")
         {
             tempYOffset += ((this._fontSize + 2) * tOffsetScaleX * quilaty);//mac 系统绘制文本是以最下角为0,0点绘制的,所以需要有一个初始化Y偏移,否则看不到第一行文本.. 2px为行间距
         }
@@ -1718,10 +1726,46 @@ class GTool{
  * */
 class OSManager{
     static get OS(){
-        return "mac";
+        OSManager.main.gOS;
     }
-    constructor(){
 
+    static get main(){
+        if(!window.gOSManager)
+            window.gOSManager = new OSManager();
+        return window.gOSManager;
+    }
+
+    constructor(){
+        this.gOS = this.detectOS();
+    }
+
+    //获取操作系统版本
+    detectOS() {
+        let sUserAgent = navigator.userAgent;
+        let isWin = (navigator.platform == "Win32") || (navigator.platform == "Windows");
+        let isMac = (navigator.platform == "Mac68K") || (navigator.platform == "MacPPC") || (navigator.platform == "Macintosh") || (navigator.platform == "MacIntel");
+        if (isMac) return "Mac";
+        let isIos = (navigator.platform == "iPhone") || (navigator.platform == "iPad")
+        if (isIos) return "IOS";
+        let isUnix = (navigator.platform == "X11") && !isWin && !isMac;
+        if (isUnix) return "Unix";
+        let isLinux = (String(navigator.platform).indexOf("Linux") > -1);
+        if (isLinux) return "Linux";
+        if (isWin) {
+            var isWin2K = sUserAgent.indexOf("Windows NT 5.0") > -1 || sUserAgent.indexOf("Windows 2000") > -1;
+            if (isWin2K) return "Win2000";
+            var isWinXP = sUserAgent.indexOf("Windows NT 5.1") > -1 || sUserAgent.indexOf("Windows XP") > -1;
+            if (isWinXP) return "WinXP";
+            var isWin2003 = sUserAgent.indexOf("Windows NT 5.2") > -1 || sUserAgent.indexOf("Windows 2003") > -1;
+            if (isWin2003) return "Win2003";
+            var isWinVista= sUserAgent.indexOf("Windows NT 6.0") > -1 || sUserAgent.indexOf("Windows Vista") > -1;
+            if (isWinVista) return "WinVista";
+            var isWin7 = sUserAgent.indexOf("Windows NT 6.1") > -1 || sUserAgent.indexOf("Windows 7") > -1;
+            if (isWin7) return "Win7";
+            var isWin10 = sUserAgent.indexOf("Windows NT 10") > -1 || sUserAgent.indexOf("Windows 10") > -1;
+            if (isWin10) return "Win10";
+        }
+        return "other";
     }
 }
 
