1dad4affdb644afea8e34f295be554683861f01c
guominglong <guominglong@51talk.com>
2017-12-26 19:25:38 +0800

    制作demo

--- HTMLLibary/js/AppDelegate.js
+++ HTMLLibary/js/AppDelegate.js
@@ -11,6 +11,27 @@ class AppDelegate{
 
     constructor(){
         this.scene = BaseScene.main;
+        this.fangxiangPanel = new GMLSprite();
+        for(let i = 0 ;i < 4;i++){
+            let spr = new GMLSprite();
+            spr.name = "fang_" + i;
+            spr.makeShape(-20,-20,40,40,0x0f660080,0);
+            this.fangxiangPanel.addChild(spr);
+            if(i == 0){
+                spr.x = -110;
+                spr.y = -20;
+            }else if(i == 1){
+                spr.x = -65;
+                spr.y = -65;
+            }else if(i == 2){
+                spr.x = -20;
+                spr.y = -20;
+            }else{
+                spr.x = -65;
+                spr.y = -20;
+            }
+            spr.addEventListener(GMLMouseEvent.Down,this.fangxiangMouseDown)
+        }
     }
 
     /**
@@ -23,102 +44,34 @@ class AppDelegate{
         //启动场景
         this.scene.start();
 
-        //测试用
-        let spr1 = new GMLSprite();
-        spr1.name = "s1"
-        spr1.makeShape(0,0,500,500,0x0f660080,0);
+        //添加动画元件
+        this.mc_dog = new GMLImage('./resource/0.png')
+        this.scene.addChild(this.mc_dog)
 
-        spr1.x = 0;
-        spr1.itiwX = spr1.itiwY = 0.5;
+        //添加方向键盘
+        this.scene.addChild(this.fangxiangPanel);
 
-        let spr2 = new GMLSprite();
-        spr2.name = "s2"
-        spr2.makeShape(0,0,100,100,0xff660080,0);
-        spr2.x = 120;
-        spr2.itiwX = spr2.itiwY = 0.5;
+        this.fangxiangPanel.x = this.scene.width;
+        this.fangxiangPanel.y = this.scene.height;
+    }
 
-        let spr3 = new GMLSprite();
-        spr3.name = "s3"
-        spr3.makeShape(0,0,100,100,0x006600ff,0x006600ff);
-        spr3.x = 230;
-        spr3.itiwX = spr3.itiwY = 0.5;
 
-        this.scene.addChild(spr1)
-        this.scene.addChild(spr2)
-        this.scene.addChild(spr3)
-        function b(evt){
-            console.log(evt.gCurrentTarget.name,evt.type);
+    fangxiangMouseDown(evt){
+        console.log(evt.gCurrentTarget.name);
+
+        if(evt.gCurrentTarget.name == "fang_0"){
+            //向左移动
+            AppDelegate.app.mc_dog.x -= 5;
+        }else if(evt.gCurrentTarget.name == "fang_1"){
+            //向上移动
+            AppDelegate.app.mc_dog.y -= 5;
+        }else if(evt.gCurrentTarget.name == "fang_2"){
+            //向右移动
+            AppDelegate.app.mc_dog.x += 5;
+        }else if(evt.gCurrentTarget.name == "fang_3"){
+            //向下移动
+            AppDelegate.app.mc_dog.y += 5;
         }
-        function onkd(evt){
-            console.log(evt.data.keyCode);
-        }
-
-
-
-        for(let i=0;i<1;i++)
-        {
-            let img = new GMLImage("./resource/0.png");
-
-            img.name = "img_" + i;
-            img.itiwX = 0.5;
-            img.itiwY = 0.5;
-
-            img.x = img.y = 300;
-            let sprtt = new GMLSprite()
-            sprtt.makeShape(0,0,300,300,0xf500f5,0xf500f5);
-            sprtt.x = 300;
-            sprtt.y = 300;
-            sprtt.itiwX = sprtt.itiwY = 0.5;
-
-            this.scene.addChild(sprtt);
-            this.scene.addChild(img);
-            ////添加键盘控制的位移动画
-            //BaseNotificationCenter.main.addObserver(sprtt,NotifyStruct.onKeyDown,function(evt){
-            //    let kecode = evt.keyCode;
-            //    switch(kecode){
-            //        case 32:sprtt.scaleX = sprtt.scaleY = 1.3;break;
-            //        case 37:sprtt.x -= 2;break;
-            //        case 39:sprtt.x += 2;break;
-            //        case 38:sprtt.y -= 2;break;
-            //        case 40:sprtt.y += 2;break;
-            //    }
-            //});
-            //
-            //BaseNotificationCenter.main.addObserver(sprtt,NotifyStruct.onKeyUp,function(evt){
-            //    let kecode = evt.keyCode;
-            //    switch(kecode){
-            //        case 32:
-            //            sprtt.scaleX = sprtt.scaleY = 1;
-            //            break;
-            //    }
-            //});
-        }
-
-        ////当ScreenManager  的 quilaty 不为1的时候, textF._的reCountResultText算法有问题
-        //let textF = new GMLStaticTextField()
-        ////textF.hasBackground = true;
-        ////textF.hasBorder = true;
-        ////textF.borderColor = 0xff6600ff;
-        ////textF.backgroundColor = 0x0000ffff;
-        //textF.fontSize = 20;
-        //textF.fontColor = 0xff0000ff;
-        //textF.width = 100;
-        //textF.height = 25;
-        //this._rootSprite.addChild(textF);
-        //textF.text = "我是中国人我是中国人我是中国人我是中国人我是中国人";
-        //textF.x = 100;
-        //textF.y = 150;
-
-        //let tempi = 0
-        //let tArr = "我是中国人".split("");
-        //setInterval(function(){
-        //    textF.text = textF.text + tArr[tempi];
-        //    tempi ++;
-        //    if(tempi > 4){
-        //        tempi = 0;
-        //    }
-        //},50);
-
     }
 
 
@@ -127,6 +80,8 @@ class AppDelegate{
      * */
     resize(w,h){
         BaseScene.main.resize(w,h)
+        this.fangxiangPanel.x = w;
+        this.fangxiangPanel.y = h;
     }
 
     /**
