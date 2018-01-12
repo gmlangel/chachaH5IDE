af1b1c3861239618bf8e91d67012707018b407b2
guominglong <guominglong@51talk.com>
2018-01-12 10:28:28 +0800

    添加A星寻路算法

--- HTMLLibary/js/Custom.js
+++ HTMLLibary/js/Custom.js
@@ -296,3 +296,143 @@ WebSocketEvent.SOCKET_ERROR = "WebSocketEvent.socket.error";
 WebSocketEvent.SOCKET_DATA = "WebSocketEvent.socket.data";
 //当socket链接成功
 WebSocketEvent.SOCKET_CONNECTED = "WebSocketEvent.socket.connected";
+
+
+/**
+ * A星寻路算法
+ * */
+class AStar extends BaseEvent{
+    static get main(){
+        if(!window.gmlaSTAR)
+            window.gmlaSTAR = new AStar();
+        return window.gmlaSTAR;
+    }
+    constructor(){
+        super();
+    }
+
+    /**
+     * 障碍物图像数据检索路径
+     * */
+    searchRoadByImgData(start_x,start_y,end_x,end_y,_imgData){
+        var openList=[],    //开启列表
+            closeList=[],   //关闭列表
+            result=[],      //结果数组
+            result_index;   //结果数组在开启列表中的序号
+
+        openList.push({x:start_x,y:start_y,G:0});//把当前点加入到开启列表中，并且G是0
+
+        do{
+            var currentPoint = openList.pop();
+            closeList.push(currentPoint);
+            var surroundPoint=this._SurroundPoint(currentPoint);
+            for(var i in surroundPoint) {
+                var item = surroundPoint[i];                //获得周围的八个点
+                //if (
+                //    item.x>=0 &&                            //判断是否在地图上
+                //    item.y>=0 &&
+                //    item.x<MAP.rows &&
+                //    item.y<MAP.cols &&
+                //    MAP.arr[item.x][item.y] != 1 &&         //判断是否是障碍物
+                //    !this._existList(item, closeList) &&          //判断是否在关闭列表中
+                //    MAP.arr[item.x][currentPoint.y]!=1 &&   //判断之间是否有障碍物，如果有障碍物是过不去的
+                //    MAP.arr[currentPoint.x][item.y]!=1) {
+                if (
+                    this._getColorByXY(_imgData.data,item.x,item.y,_imgData.width,0) > 1 &&         //判断是否是障碍物
+                    !this._existList(item, closeList) &&          //判断是否在关闭列表中
+                    this._getColorByXY(_imgData.data,item.x,currentPoint.y,_imgData.width,0) > 1 &&   //判断之间是否有障碍物，如果有障碍物是过不去的
+                    this._getColorByXY(_imgData.data,currentPoint.x,item.y,_imgData.width,0) > 1) {
+                    //g 到父节点的位置
+                    //如果是上下左右位置的则g等于10，斜对角的就是14
+                    var g = currentPoint.G + ((currentPoint.x - item.x) * (currentPoint.y - item.y) == 0 ? 10 : 14);
+                    if (!this._existList(item, openList)) {       //如果不在开启列表中
+                        //计算H，通过水平和垂直距离进行确定
+                        item['H'] = Math.abs(end_x - item.x) * 10 + Math.abs(end_y - item.y) * 10;
+                        item['G'] = g;
+                        item['F'] = item.H + item.G;
+                        item['parent'] = currentPoint;
+                        openList.push(item);
+                    }
+                    else {                                  //存在在开启列表中，比较目前的g值和之前的g的大小
+                        var index = this._existList(item, openList);
+                        //如果当前点的g更小
+                        if (g < openList[index].G) {
+                            openList[index].parent = currentPoint;
+                            openList[index].G = g;
+                            openList[index].F=g+openList[index].H;
+                        }
+
+                    }
+                }
+            }
+            //如果开启列表空了，没有通路，结果为空
+            if(openList.length==0) {
+                break;
+            }
+            openList.sort(this._sortF);//这一步是为了循环回去的时候，找出 F 值最小的, 将它从 "开启列表" 中移掉
+        }while(!(result_index=this._existList({x:end_x,y:end_y},openList)));
+
+        //判断结果列表是否为空
+        if(!result_index) {
+            result=[];
+        }
+        else {
+            var currentObj=openList[result_index];
+            do{
+                //把路劲节点添加到result当中
+                result.unshift({
+                    x:currentObj.x,
+                    y:currentObj.y
+                });
+                currentObj=currentObj.parent;
+            }while (currentObj.x!=start_x || currentObj.y!=start_y);
+
+        }
+        return result;
+
+    }
+
+    //用F值对数组排序
+    _sortF(a,b){
+        return b.F- a.F;
+    }
+
+    //获取周围八个点的值
+    _SurroundPoint(curPoint){
+        var x=curPoint.x,y=curPoint.y;
+        return [
+            {x:x-1,y:y-1},
+            {x:x,y:y-1},
+            {x:x+1,y:y-1},
+            {x:x+1,y:y},
+            {x:x+1,y:y+1},
+            {x:x,y:y+1},
+            {x:x-1,y:y+1},
+            {x:x-1,y:y}
+        ]
+    }
+    //判断点是否存在在列表中，是的话返回的是序列号
+    _existList(point,list) {
+        for(var i in list) {
+            if(point.x==list[i].x && point.y==list[i].y) {
+                return i;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 获取位图上,指定x,y坐标点ARGB中的一个色值,比如取50,50坐标点的红色色值
+     * @param _imgData位图数据  ImageData.Data
+     * @param _imgX X坐标
+     * @param _imgY Y坐标
+     * @param _lineLength   指_imgData位图宽度
+     * @param _offset 0可以取R值, 1可以取G值,2可以去B值,3可以去Alpha值
+     * */
+    _getColorByXY(_imgData,_imgX,_imgY,_lineLength,_offset){
+        let ty = parseInt(_imgY);
+        let tx = parseInt(_imgX);
+        let tlen = parseInt(_lineLength);
+        return _imgData[ty * tlen + tx * 4 + _offset] || 0;
+    }
+}
